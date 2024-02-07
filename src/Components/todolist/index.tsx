c6125bff8dc2b5977594e65cee43ff1a67e85307
kow2004 <kowsalyakavitha64@gmail.com>
2024-02-07 18:12:46 +0530

    created pageination

--- src/Components/todolist/index.tsx
+++ src/Components/todolist/index.tsx
@@ -1,29 +1,49 @@
-import React from 'react'
-import Styles  from './index.module.scss'
-import { inputtype } from '../../types/inputtype'
+import React, { useState } from 'react';
+import Styles from './index.module.scss';
+import { inputtype } from '../../types/inputtype';
 
-export type todos={
-    todo:inputtype[],
+export type todos = {
+    todo: inputtype[];
     settodo: React.Dispatch<React.SetStateAction<inputtype[]>>;
-}
-export const TodoList:React.FC<todos> = ({todo,settodo}) => {
+};
+
+export const TodoList: React.FC<todos> = ({ todo, settodo }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const todosPerPage = 3;
+
+    const indexOfLastTodo = currentPage * todosPerPage;
+    const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+    const currentTodos = todo.slice(indexOfFirstTodo, indexOfLastTodo);
 
     const deleteTodo = (index: number) => {
-        settodo(todo.filter((_, i) => i !== index));
-      };
-    
-  return (
-      
-    <div className={Styles.todolist}> 
-    {todo.map((item,index)=>(
-   <div key={index}>
-    <div className={Styles.todoitem} >
-       <div className={Styles.item}>{item.dos}</div>
-       <div>{item.deadline}</div>
-       <button className={Styles.deletebutton } onClick={() => deleteTodo(index)}>Delete</button>
-    </div>
-    </div> 
-    ))}
-   </div>
-  )
-}
+        const updatedTodos = [...todo.slice(0, index), ...todo.slice(index + 1)];
+        settodo(updatedTodos);
+    };
+
+    const paginate = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
+    };
+
+    return (
+        <div className={Styles.todolist}>
+            {currentTodos.map((item, index) => (
+                <div key={index}>
+                    <div className={Styles.todoitem}>
+                        <div className={Styles.item}>{item.dos}</div>
+                        <div>{item.deadline}</div>
+                        <button className={Styles.deletebutton} onClick={() => deleteTodo(index)}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            ))}
+            <div className={Styles.pagination}>
+                {Array.from({ length: Math.ceil(todo.length / todosPerPage) }, (_, i) => (
+                    <button key={i} onClick={() => paginate(i + 1)}>
+                        {i + 1}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+};
